77d3c037ec324a35dfe3bb7bc9e6c73bbda52908
Antonio Filho <antonio.filho@mult-e.com.br>
2018-03-28 10:26:47 -0300

    Minor modifications

--- googleclient/src/main/java/com/example/googleclient/GoogleclientApplication.java
+++ googleclient/src/main/java/com/example/googleclient/GoogleclientApplication.java
@@ -2,7 +2,12 @@ package com.example.googleclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.LinkedHashMap;
 
 @SpringBootApplication
 @RestController
@@ -12,14 +17,14 @@ public class GoogleclientApplication {
 		SpringApplication.run(GoogleclientApplication.class, args);
 	}
 
-//	@GetMapping("/user")
-//	public Principal user(Principal user){
-//		return user;
-//	}
-//
-//	@GetMapping("/resource")
-//	public String getProtectedResource(OAuth2Authentication authentication){
-//		LinkedHashMap<String, String> details = (LinkedHashMap<String, String>)authentication.getUserAuthentication().getDetails();
-//		return String.format("%s, you are allowed to access this protected resource", details.get("name"));
-//	}
+	@GetMapping("/user")
+	public Principal user(Principal user){
+		return user;
+	}
+
+	@GetMapping("/resource")
+	public String getProtectedResource(OAuth2Authentication authentication){
+		LinkedHashMap<String, String> details = (LinkedHashMap<String, String>)authentication.getUserAuthentication().getDetails();
+		return String.format("%s, you are allowed to access this protected resource", details.get("name"));
+	}
 }
--- googleclient/src/main/java/com/example/googleclient/SecurityConfiguration.java
+++ googleclient/src/main/java/com/example/googleclient/SecurityConfiguration.java
@@ -1,23 +1,29 @@
 package com.example.googleclient;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
 /**
  * Created by mult-e on 27/06/2017.
  */
-//@Configuration
-//@EnableOAuth2Sso
-public class SecurityConfiguration //extends WebSecurityConfigurerAdapter
+@Configuration
+@EnableOAuth2Sso
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 {
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .antMatcher("/**")
-//                .authorizeRequests()
-//                .antMatchers("/", "/index.html")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated().and()
-//                .csrf()
-//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/index.html")
+                .permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
 }
